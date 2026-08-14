5ad5ab6df4ade11dbac8debee7690701a8afb01f
Nikita <102962775+13PurpleHaze@users.noreply.github.com>
2026-08-14 10:49:10 +0500

    feature/main update base.py

--- src/infrastructure/db/models/base.py
+++ src/infrastructure/db/models/base.py
@@ -1,10 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase
 
 
-## Создает объект MetaData под капотом
-## repr - метод для читаемого отображения
-
-
 class Base(DeclarativeBase):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.__dict__})"
